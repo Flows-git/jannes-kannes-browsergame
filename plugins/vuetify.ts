8d284727fb1492b5f7e33c183aca0a71e91f787f
Flo <f.wagner92@gmail.com>
2025-05-30 12:50:24 +0200

    update data

--- plugins/vuetify.ts
+++ plugins/vuetify.ts
@@ -1,24 +1,27 @@
+import type { ThemeDefinition } from 'vuetify'
+import { createVuetify } from 'vuetify'
+
 // import this after install `@mdi/font` package
 import '@mdi/font/css/materialdesignicons.css'
-
 import 'vuetify/styles'
-import { type ThemeDefinition, createVuetify } from 'vuetify'
 
 const b2wTheme: ThemeDefinition = {
-  dark: false,
+  dark: true,
   colors: {
-    background: '#090909',
-    surface: '#212121',
-    primary: '#FBCF3B',
-    secondary: '#03DAC6',
-    error: '#B00020',
-    info: '#2196F3',
-    success: '#4CAF50',
-    warning: '#FB8C00',
-    undead: '#722F90',
-    orc: '#C51C28',
-    human: '#21A4EB',
-    nightelf: '#05953B',
+    'background': '#090909',
+    'surface': '#212121',
+    'surface-variant': '#424242',
+    'on-surface-variant': '#FFFFFF',
+    'primary': '#FBCF3B',
+    'secondary': '#03DAC6',
+    'error': '#B00020',
+    'info': '#2196F3',
+    'success': '#4CAF50',
+    'warning': '#FB8C00',
+    'undead': '#722F90',
+    'orc': '#C51C28',
+    'human': '#21A4EB',
+    'nightelf': '#05953B',
   },
 }
 
--- scripts/parseTagsCsvToJson.ts
+++ scripts/parseTagsCsvToJson.ts
@@ -2,11 +2,11 @@ import { parseCsvToJson } from './helper/parseCsvToJson'
 
 export async function parseTagsCsvToJson() {
   parseCsvToJson<TagCsv, GameTag>('tags', (tag) => {
-    const icon = tag.icon?.replace('.png', '')
+    const icon = tag.icon
 
     return {
       name: tag.name,
-      icon: icon ? `${icon}.png` : undefined,
+      icon: icon || undefined,
       reforgedIcon: icon ? `${icon}-Reforged.png` : undefined,
       mainAttributes: tag.main_attributes?.split(',').filter(t => t),
     }
