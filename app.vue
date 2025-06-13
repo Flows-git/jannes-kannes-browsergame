2cbcb8835ce44c727b387eb4c3057f4e92da26ca
Flo <f.wagner92@gmail.com>
2025-06-13 21:09:39 +0200

    add footer and favicon

--- app.vue
+++ app.vue
@@ -1,7 +1,15 @@
 <template>
   <NuxtLayout>
     <v-app>
-      <NuxtPage />
+      <div class="fill-height">
+        <NuxtPage />
+      </div>
+      <v-footer height="36px">
+        <v-spacer />
+        Made with <v-icon icon="mdi-heart" class="mx-1" color="error" /> by <NuxtLink class="pl-1" href="https://github.com/Flows-git" target="_blank">
+          Florian Wagner
+        </NuxtLink>
+      </v-footer>
     </v-app>
   </NuxtLayout>
 </template>
