2384b54ebfb132813a7414e6f8c29a7d6740bc53
Flo <f.wagner92@gmail.com>
2025-09-23 19:29:03 +0200

    update leaderboard ui

--- app/pages/leaderboard.vue
+++ app/pages/leaderboard.vue
@@ -1,8 +1,10 @@
 <script setup lang="ts">
 import type { DataTableHeader } from 'vuetify'
+import { useDisplay } from 'vuetify'
 
 const route = useRoute()
 const router = useRouter()
+const { xs } = useDisplay()
 
 const id: number | undefined = route.query.id ? Number(route.query.id) : undefined
 const perPage = ref(Number(route.query.perPage as string ?? 10))
@@ -26,13 +28,23 @@ const { data, pending } = useFetch<{ items: Array<LeaderboardListEntry>, meta: {
   return `/api/leaderboard?page=${page.value}&perPage=${perPage.value}`
 })
 
-const headers: Array<DataTableHeader> = [
-  { title: 'Rang', key: 'rank', align: 'center', sortable: false, maxWidth: 100, width: 100 },
-  { title: 'Name', key: 'name', sortable: false },
-  { title: 'Punktzahl', key: 'score', align: 'center', sortable: false, maxWidth: 100, width: 100 },
-  { title: 'Spielzeit', key: 'gameTime', align: 'center', sortable: false, maxWidth: 150, width: 150 },
-  { title: 'Durchschnittliche Antwortzeit', align: 'center', key: 'averageAnswerTime', sortable: false, maxWidth: 150, width: 150 },
-]
+const headers = computed<Array<DataTableHeader>>(() => {
+  const i: Array<DataTableHeader> = [
+    { title: 'Rang', key: 'rank', align: 'center', sortable: false, maxWidth: 100, width: 100 },
+    { title: 'Name', key: 'name', sortable: false },
+    { title: 'Punktzahl', key: 'score', align: 'center', sortable: false, maxWidth: 100, width: 100 },
+
+  ]
+
+  if (!xs.value) {
+    i.push(
+      { title: 'Spielzeit', key: 'gameTime', align: 'center', sortable: false, maxWidth: 150, width: 150 },
+      { title: 'Durchschnittliche Antwortzeit', align: 'center', key: 'averageAnswerTime', sortable: false, maxWidth: 150, width: 150 },
+    )
+  }
+
+  return i
+})
 
 const confettiDone = ref(false)
 const confetti = useConfetti()
@@ -79,6 +91,18 @@ function isRowSelected(data: LeaderboardListEntry) {
           <span class="font-weight-bold text-h6">
             {{ value }}
           </span>
+        </template>
+        <template #no-data>
+          <div class="pa-4">
+            <v-icon icon="mdi-trophy-broken" color="primary" size="80" />
+            <div class="text-h5 py-3">
+              Keine Einträge
+            </div>
+            <div>Sei der erste und</div>
+            <v-btn color="primary" variant="outlined">
+              Starte ein Ranglistenspiel
+            </v-btn>
+          </div>
         </template>
       </v-data-table-server>
     </v-card>
