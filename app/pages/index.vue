dd5c0923284df3220ed1b275eaf7a73f781336e7
Flo Wagner <f.wagner92@gmail.com>
2026-04-02 21:34:49 +0200

    feat: refactor main page

--- app/pages/index.vue
+++ app/pages/index.vue
@@ -16,81 +16,88 @@ async function startNewGame(mode: GameMode) {
     loading.value = false
   }
 }
+
+const selectedGameMode = ref()
+const gameModes = [
+  {
+    mode: 'classic',
+    icon: 'mdi-controller-classic-outline',
+    label: 'Schnelles Spiel',
+    info: `Eine klassische Runde 'Jannes Kann es'. Es werden drei Fragen aus 'Jannes Kann es' gestellt und das Spiel ist gewonnen wenn alle Fragen richtig beantwortet wurden. \nEs gibt natürlich einen Preis!`,
+  },
+  {
+    mode: 'endless',
+    icon: 'mdi-infinity',
+    label: 'Endlos Modus',
+    info: `Casual Mode. \nEs werden alle "Jannes Kann es" Fragen gestellt`,
+  },
+  {
+    mode: 'ranked',
+    icon: 'mdi-trophy-variant-outline',
+    label: 'Ranglistenspiel',
+    info: `Es werden alle "Jannes Kann es" Fragen gestellt. Man hat drei Leben. \nDas Spiel ist vorbei wenn man keine Leben mehr hat. Das Ergebnis kann in eine Rangliste eingetragen werden.`,
+  },
+]
 </script>
 
 <template>
-  <v-container max-width="800">
-    <v-card color="surface-variant">
-      <v-card-text class="text-center">
-        <GameLogo show-subtitle width="300" />
-        <v-alert color="primary" type="info" variant="tonal" density="compact" icon="mdi-heart" class="mt-4">
-          Das inoffizielle Spiel zum beliebten Format "Jannes Kann Es" aus der Sendung Creepjack.
-          Alle Fragen stammen aus der Creepjack Community!
-        </v-alert>
-      </v-card-text>
-      <v-progress-linear color="primary" :indeterminate="loading" />
-    </v-card>
+  <v-container max-width="1200px">
+    <div>
+      <GameLogo show-subtitle width="500" />
+    </div>
+    <v-item-group v-model="selectedGameMode" selected-class="border-primary border-opacity-100 selected-card">
+      <v-row justify="center" class="pt-4">
+        <v-col v-for="mode of gameModes" :key="mode.mode" cols="12" sm="6" md="4">
+          <v-item v-slot="{ isSelected, selectedClass, select }" :value="mode">
+            <v-card :class="selectedClass" class="pa-3 text-center border-md border-opacity-0" :color="isSelected ? 'surface-variant' : ''" @click="select">
+              <v-btn
+                v-tooltip="{
+                  text: mode.info,
+                  width: 300,
+                  openOnClick: true,
+                  contentClass: 'wrapped-text',
+                }" icon="mdi-information-outline" variant="text"
+                style="position: absolute; right: 4px; top: 4px;"
+              />
+              <v-avatar color="primary" size="94">
+                <v-icon :icon="mode.icon" size="86" />
+              </v-avatar>
+              <div class="text-h5 pt-2">
+                {{ mode.label }}
+              </div>
+            </v-card>
+          </v-item>
+        </v-col>
+      </v-row>
+    </v-item-group>
+    <div class="text-center pt-4">
+      <v-btn size="x-large" color="primary" :loading="loading" :disabled="!selectedGameMode" @click="startNewGame(selectedGameMode.mode)">
+        Spiel Starten
+      </v-btn>
 
+      <v-alert color="primary" type="info" variant="tonal" density="compact" icon="mdi-heart" class="mt-4">
+        Das inoffizielle Spiel zum beliebten Format "Jannes Kann Es" aus der Sendung Creepjack.
+        Alle Fragen stammen aus der Creepjack Community!
+      </v-alert>
+    </div>
     <v-row class="pt-4">
       <v-col cols="12">
-        <v-card class="fill-height" :disabled="loading" data-testid="quick-game-btn" @click="startNewGame('classic')">
-          <v-card-title>
-            <v-icon icon="mdi-controller-classic-outline" color="primary" size="24" />
-
-            Schnelles Spiel
-          </v-card-title>
-          <v-card-text>
-            Eine klassische Runde "Jannes Kann es". Es werden drei Fragen aus "Jannes Kann es" gestellt und das Spiel ist gewonnen wenn alle Fragen
-            richtig beantwortet wurden. Es gibt natürlich einen Preis!
-          </v-card-text>
-        </v-card>
-      </v-col>
-      <v-col cols="12" sm="6">
-        <v-card class="fill-height" :disabled="loading" data-testid="endless-game-btn" @click="startNewGame('endless')">
-          <v-card-title>
-            <v-icon icon="mdi-infinity" color="primary" size="24" />
-            Endlos Modus
-          </v-card-title>
-          <v-card-text>Es werden alle "Jannes Kann es" Fragen gestellt.</v-card-text>
-        </v-card>
-      </v-col>
-      <v-col cols="12" sm="6">
-        <v-card class="fill-height" :disabled="loading" data-testid="ranked-game-btn" @click="startNewGame('ranked')">
-          <v-card-title>
-            <v-icon icon="mdi-trophy-variant-outline" color="primary" size="24" />
-            Ranked
-          </v-card-title>
-          <v-card-text>
-            Es werden alle "Jannes Kann es" Fragen gestellt. Man hat drei Leben.
-            Das Spiel ist vorbei wenn man keine Leben mehr hat. Das Ergebnis kann in eine Rangliste eingetragen werden.
-          </v-card-text>
-        </v-card>
-      </v-col>
-      <v-col cols="12">
-        <v-card class="fill-height d-flex align-center pa-3" color="surface-variant" :disabled="loading" to="/leaderboard">
+        <v-card class="fill-height d-flex align-center pa-3" color="surface" :disabled="loading" to="/leaderboard">
           <v-icon icon="mdi-podium" color="primary" size="60" />
           <v-card-title>Bestenliste</v-card-title>
         </v-card>
       </v-col>
-      <!-- <v-col cols="12" sm="6">
-        <v-card class="fill-height" disabled>
-          <v-card-title>Eigenes Spiel</v-card-title>
-          <v-card-text>
-            Erstelle ein "Jannes Kann es" Spiel nach deinen Wünschen. Spiele den klassischen oder endlos Modus, bestimme die Anzahl der Fragen oder
-            Fehlversuche...
-          </v-card-text>
-        </v-card>
-      </v-col> -->
     </v-row>
-
-    <v-snackbar v-model="error" color="error" class="mb-12" timer="8000" variant="flat">
-      Beim Starten des Spiels ist ein Fehler aufgetreten. Versuche es später erneut.
-      <div class="text-center opacity-50">
-        {{ error?.message }}
-      </div>
-      <template #actions="{ isActive }">
-        <v-btn icon="mdi-close" @click="isActive.value = false" />
-      </template>
-    </v-snackbar>
   </v-container>
 </template>
+
+<style>
+.wrapped-text {
+  white-space: pre-wrap;
+}
+
+.selected-card {
+  box-shadow: 0px 0px 32px rgb(var(--v-theme-primary)) !important;
+  transition: boxshadow 1s ease;
+}
+</style>
