3889c3184b4904e887c1c737343d8ff897904cab
Flo Wagner <f.wagner92@gmail.com>
2026-04-09 13:07:07 +0200

    fix: setup script

--- scripts/_setup.ts
+++ scripts/_setup.ts
@@ -1,8 +1,5 @@
 import { existsSync } from 'node:fs'
 import { downloadUnzipAndDeleteFile } from './helper/download'
-import { upsertJsonFile } from './helper/upsertJsonFile'
-// import { parseQuestionsCsvToJson } from './parseQuestionsCsvToJson'
-import { parseTagsCsvToJson } from './parseTagsCsvToJson'
 
 if (!existsSync('public/warcraft3_icons')) {
   downloadUnzipAndDeleteFile('https://wc3icons.coffbox.win/assets/warcraft3_icons.zip', 'warcraft3_icons.zip')
@@ -10,13 +7,3 @@ if (!existsSync('public/warcraft3_icons')) {
 else {
   console.log(`Warcraft 3 Icons already downloaded and unzipped!`)
 }
-
-// const questions = await parseQuestionsCsvToJson()
-// await upsertJsonFile('questions', questions)
-// console.log(`questions.csv is parsed to JSON`)
-
-const tags = await parseTagsCsvToJson()
-await upsertJsonFile('tags', tags)
-console.log(`tags.csv is parsed to JSON`)
-
-// await setupSupabaseDB()
